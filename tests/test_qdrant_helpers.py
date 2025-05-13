4905846795b32b0975d8f0716a7bf5d25b05de46
Alberto Rivas II <alberto.rivasii.2024@marshall.usc.edu>
2025-05-13 16:05:26 -0700

    write first qdrant helper test

--- tests/test_qdrant_helpers.py
+++ tests/test_qdrant_helpers.py
@@ -1,11 +1,16 @@
-from utils.qdrant_helpers import *
+from qdrant_client import QdrantClient
+from utils.qdrant_helpers import QdrantHelper
 import numpy as np
 
 def test_qdrant_flow():
     client= QdrantClient(host="localhost", port=6333)
     col_name= "test_collection"
-    creation_result= create_collection(col_name, 384)
-    assert creation_result == f"Collection Created. Name: {col_name}"
+    db= QdrantHelper(client)
+    creation_result= db.create_collection(col_name, 384)
+    assert creation_result == f"Collection Created. Name: {col_name}", creation_result
+    
+    collections= db.list_collections()
+    assert col_name in collections, collections
 
     # create dummy data
     embeddings= np.random.rand(5, 384)
@@ -17,12 +22,12 @@ def test_qdrant_flow():
               ]
     
     # upsert dummy data
-    upsert_result= upsert_embeddings(col_name, embeddings, payload)
-    assert upsert_result == "Upsert Complete."
+    upsert_result= db.upsert_embeddings(col_name, embeddings, payload)
+    assert upsert_result == "Upsert Complete.", upsert_result
     
     # test delete collection function at the end
-    deletion_result= delete_collection(col_name)
-    assert deletion_result == f"Collection {col_name} deleted."
+    deletion_result= db.delete_collection(col_name)
+    assert deletion_result == f"Collection {col_name} deleted.", deletion_result
 
     client.close()
     
