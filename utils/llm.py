92cf3c7a7abe2f4c2813f7deed6cc2e3703753f8
Alberto Rivas II <alberto.rivasii.2024@marshall.usc.edu>
2025-05-19 21:16:58 -0700

    llm is successfully generating output.

--- utils/llm.py
+++ utils/llm.py
@@ -1,5 +1,5 @@
 import torch
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from huggingface_hub import login
 from dotenv import load_dotenv
 import os
@@ -8,17 +8,22 @@ from typing import Union, List
 load_dotenv()
 
 class LLMHelper:
-    def __init__(self, model_id:str= "meta-llama/Llama-3.1-8B", model_type:str="pipeline", token=None):
+    def __init__(self, model_id:str= "RedHatAI/Meta-Llama-3-8B-Instruct-quantized.w4a16", model_type:str="hf", token=None, bnb_config:BitsAndBytesConfig=None):
         self.model_id= model_id
         self.model_type= model_type
+        self.config= bnb_config
+
+        if model_type == "hf":
+            self.tokenizer= AutoTokenizer.from_pretrained(model_id, token=token, device_map="auto")
+            self.model= AutoModelForCausalLM.from_pretrained(model_id, token=token, device_map="auto") #quantization_config=self.bnb_config)
         
-        if model_type == "pipeline":
+        elif model_type == "pipeline":
             self.model= pipeline("text-generation",
                                  model=model_id,
-                                 token=os.getenv("HF_API_TOKEN"),
                                  model_kwargs={"torch_dtype":torch.bfloat16},
+                                 quantization_config=bnb_config,
                                  device_map='auto')
-        
+
 
     def create_prompt(self, context:str, query:str) -> str:
         """
@@ -30,12 +35,12 @@ class LLMHelper:
         Returns:
             str: String with context and query formatting.
         """
-        if self.model_type == "mistral":
+        if "mistral" in self.model_id.lower():
             full_prompt= [
                 {"role":"system", "content": "you are a helpful assistant that answers the user's question considering the following context:\n %s" % context}
             ]
             return full_prompt
-        if self.model_type == "llama":
+        if "llama" in self.model_id.lower():
 
             full_prompt= \
             """
@@ -61,19 +66,45 @@ class LLMHelper:
         Returns:
             str: The model's first choice answer as a string.
         """
-        if "mistral" in self.model_id:
-            output= self.model(prompt)
-            return output
-        elif "llama" in self.model_id:
-            # tokenize the input prompt
-            input= self.tokenizer(prompt, return_tensors="pt").to(self.model.device)
-            output= self.model.generate(**input, max_new_tokens=2048)
+        assert isinstance(prompt, (str, list)), f"Prompt is not viable. {prompt}"
+        # if "mistral" in self.model_id.lower():
+        #     output= self.model(prompt)
+        #     return output
+        # elif "llama" in self.model_id.lower():
+        inputs= self.tokenizer(prompt, return_tensors='pt')
+        inputs= {k:v.to(self.model.device) for k, v in inputs.items()}
+        print(f"[DEBUG]: Inputs are on device {inputs['input_ids'].device}")
+        output_ids = self.model.generate(
+            **inputs,
+            max_new_tokens=200,
+            do_sample=True,
+            temperature=0.7,
+            top_p=0.95,
+            repetition_penalty=1.1
+        )
+        print(f"[DEBUG]: Outputs generated on {output_ids.device}")
 
-            return self.tokenizer.decode(output[0], skip_special_tokens=True)
-        elif self.model_type == "other":
-            pass
+        # Slice out the generated part
+        generated_tokens = output_ids[0][inputs['input_ids'].shape[-1]:]
 
+        # Decode only the new tokens
+        answer = self.tokenizer.decode(generated_tokens, skip_special_tokens=True)
+        return answer
+        # elif self.model_type == "other":
+        #     pass
 
-llm= LLMHelper(token=os.getenv("HF_API_TOKEN"))
+#login(token=os.environ.get("HF_TOKEN"))
 
-llm.generate_answer("Tell me a fun fact about Large Language Models")
+bnb_config= BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_compute_dtype=torch.bfloat16,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4"
+        )
+assert torch.cuda.is_available(), "Cuda is not available."
+
+llm= LLMHelper(model_id="RedHatAI/Meta-Llama-3-8B-Instruct-quantized.w4a16", model_type="hf", token=os.getenv("HF_TOKEN"), bnb_config=bnb_config)
+llm_prompt= llm.create_prompt("Nitamonkey is corny.", "What adjective would you use to describe Nitamonkey?")
+answer= llm.generate_answer(llm_prompt)
+print(answer)
+
